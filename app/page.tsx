dc485f5f6ff94e52bbf72dd9ae40167aae287dc4
Tomas Birbe <tomas.birbe@gmail.com>
2024-09-29 22:23:03 -0300

    feat: add sections for home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,12 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <main>Hello, World!</main>;
+  return (
+    <main>
+      <h1 className="font-bold text-xl">Home</h1>
+      <Link href={"/browse"} className="underline-offset-4 hover:underline" prefetch={true}>
+        Go to main page
+      </Link>
+    </main>
+  );
 }
