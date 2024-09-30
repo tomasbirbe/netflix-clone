b49d09e32ca3a2cb78de40cecfe4c9d4c2020c4a
Tomas Birbe <tomas.birbe@gmail.com>
2024-09-29 22:40:22 -0300

    refactor; improve html structure

--- app/browse/layout.tsx
+++ app/browse/layout.tsx
@@ -3,27 +3,5 @@ export default function Layout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return (
-    <div className="grid grid-rows-[68px_745px_auto]">
-      <nav className="flex gap-7">
-        <p>Logo</p>
-        <div className="flex justify-between w-full">
-          <ul className="flex gap-3">
-            <li>Inicio</li>
-            <li>Series</li>
-            <li>Peliculas</li>
-            <li>Novedades populares</li>
-            <li>Mi lista</li>
-            <li>Explora por idiomas</li>
-          </ul>
-          <ul className="flex gap-3">
-            <li>Buscar</li>
-            <li>Notificaciones</li>
-            <li>Perfil</li>
-          </ul>
-        </div>
-      </nav>
-      {children}
-    </div>
-  );
+  return <div className="grid grid-rows-[745px_auto]">{children}</div>;
 }
--- app/browse/page.tsx
+++ app/browse/page.tsx
@@ -1,7 +1,26 @@
 export default function Home() {
   return (
     <>
-      <header>Recomendacion</header>
+      <header>
+        <nav className="flex gap-[43px] items-center px-[4%] text-[14px] h-[68px]">
+          <p className="min-w-[92.5px]">Logo</p>
+          <div className="flex justify-between w-full">
+            <ul className="flex gap-[18px]">
+              <li className="font-medium">Inicio</li>
+              <li>Series</li>
+              <li>Peliculas</li>
+              <li>Novedades populares</li>
+              <li>Mi lista</li>
+              <li>Explora por idiomas</li>
+            </ul>
+            <ul className="flex gap-3">
+              <li>Buscar</li>
+              <li>Notificaciones</li>
+              <li>Perfil</li>
+            </ul>
+          </div>
+        </nav>
+      </header>
       <main>Hello, World!</main>
     </>
   );
